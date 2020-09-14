03818740fe4cbf64cf99d82c520b509b65fb713e
wodyd7017 <wodyd7017@nate.com>
2020-09-14 09:06:22 +0900

    Updates

---
+++ precache-manifest.63c07d9c48eff54b1fd77e5c0e3e85ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a3f06f667c549c88ca34fe81b3a0d0d",
+    "url": "/test_react_app/index.html"
+  },
+  {
+    "revision": "43203aa1c50388749cdc",
+    "url": "/test_react_app/static/css/main.565d7cb7.chunk.css"
+  },
+  {
+    "revision": "fd2b54e72b577e61bd18",
+    "url": "/test_react_app/static/js/2.82b8a2e0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test_react_app/static/js/2.82b8a2e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43203aa1c50388749cdc",
+    "url": "/test_react_app/static/js/main.4009d2f3.chunk.js"
+  },
+  {
+    "revision": "03770262adf5b3ccbceb",
+    "url": "/test_react_app/static/js/runtime-main.3b96ff47.js"
+  }
+]);
